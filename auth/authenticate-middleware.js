ab58602dec2a5495804075a91891e044358af0b7
wildcard329 <enkiduwildman@gmail.com>
2020-04-24 09:02:17 -0700

    routes are working correctly

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,23 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
+
+const secrets = require('../api/secrets.js');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  const secret = secrets.jwtSecret;
+
+  if(token) {
+    jwt.verify(token, secret, (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({ you: 'shall not pass!' });
+      } else {
+        req.decodedToken - decodedToken;
+
+        next();
+      }
+    })
+  } else {
+    res.status(400).json({message: 'invalid credentials' })
+  }
 };
